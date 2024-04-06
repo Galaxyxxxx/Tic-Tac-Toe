3af40793c13719d7153e99c88ac9e82f27bd3e1b
Galaxy <jactyl13@gmail.com>
2024-04-06 17:42:25 +0200

    command not working ig

--- Main.py
+++ Main.py
@@ -8,28 +8,33 @@ customtkinter.set_default_color_theme("dark-blue")
 #App frame
 app = customtkinter.CTk()
 app.geometry("1280x720")
+app.minsize(width = 1280, height = 720)
 app.title("Tic-Tac-Toe")
-app._window = customtkinter.CTk()
+
 
 #Ui elements
 mainfont = customtkinter.CTkFont(family= "Arial", size = 20, weight = "bold")
 title = customtkinter.CTkLabel(app, text = "Tic-Tac-Toe", font = mainfont)
 title.pack(pady = 30)
 
+#Game logic
+def handleplayermove(col, row):
+    print("l", col, row)
+    
+    
 #Tic-Tac-Toe board
-board = customtkinter.CTkFrame(master = app, width = 750, height = 450, bg_color = "white", border_width= 5, border_color = "white")
+board = customtkinter.CTkFrame(master = app, fg_color = "transparent", border_width= 0, border_color = "white")
+board.pack(expand=True, fill="both", padx=50, pady=50)
 buttons = []
 for row in range(3):
     for col in range(3):
-        btn = customtkinter.CTkButton(board, text="", width= 250, height=150, fg_color= "transparent", border_width= 5, border_color = "white", font = mainfont)
+        btn = customtkinter.CTkButton(board, fg_color= "#e6e6e6", border_width= 0, border_color = "white", font = mainfont, corner_radius = 0, hover_color= "#cccccc", command=handleplayermove(col, row))
         buttons.append(btn)
-        btn.grid(row=row, column=col, sticky="nsew")
-        
-board.update_idletasks()
-width = board.winfo_width()
-height = board.winfo_height()
-app._window.geometry(f"{width}x{height}")
-board.pack(padx = 50)
+        btn.grid(row=row, column=col, sticky="nsew", padx=10, pady=10)
+        board.grid_columnconfigure(col, weight=1)
+        board.grid_rowconfigure(row, weight=1)
 
 #App run
 app.mainloop()
+
+
